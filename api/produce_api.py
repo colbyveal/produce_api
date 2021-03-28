5aa01b066a1bde17bb03fecf71297648f7d14af4
Colby Veal <colbydanielle220@gmail.com>
2021-03-27 21:31:16 -0400

    Added more verification to post command and comments

--- api/produce_api.py
+++ api/produce_api.py
@@ -5,15 +5,19 @@ from random import choices
 
 app = Flask (__name__)
 api = Api(app)
+parser = reqparse.RequestParser()
 
-"""PRODUCE - TYPE: dict
+"""
+PRODUCE - TYPE: dict
     Key: 
-        produce code: 19 char string consisting of 4 dash seperated 4 character strings. Alphanumeric and case insensitive
+        produce code: 16 char string consisting of 4 'dash-seperated' 4 character strings (19 chars total). Alphanumeric and case insensitive
     Value:
-        produce code: 16 char string consisting of 4 dash seperated 4 character strings. Alphanumeric and case insensitive
-            use generateProduceCode() to generate a unique code when POSTing new produce
-        name: Case insensitive alphanumeric string
-        price: number with exactly 2 decimal places
+        stuct:
+            produce code: 16 char string consisting of 4 dash seperated 4 character strings. Alphanumeric and case insensitive
+                use generateProduceCode() to generate a unique code when POSTing new produce
+                not passed in via api-call
+            name: Case insensitive alphanumeric string
+            price: number with exactly 2 decimal places
 """
 PRODUCE = {
     'A12T-4GH7-QPL9-3N4M': {'produce code': 'A12T-4GH7-QPL9-3N4M', 'name': 'Lettuce', 'price': 3.46},
@@ -22,23 +26,40 @@ PRODUCE = {
     'TQ4C-VV6T-75ZX-1RMR': {'produce code': 'TQ4C-VV6T-75ZX-1RMR', 'name': 'Gala  Apple', 'price': 3.59}
 }
 
-parser = reqparse.RequestParser()
-
+"""
+generateProduceCode will generate a unique code for each produce in our dict
+    produce code: 16 char string consisting of 4 'dash-seperated' 4 character strings (19 chars total). Alphanumeric and case insensitive
+    example: A12T-4GH7-QPL9-3N4M
+"""
 def generateProduceCode():
     codeChunk1 = ''.join(choices(ascii_uppercase+digits, k=4));
     codeChunk2 = ''.join(choices(ascii_uppercase+digits, k=4));
     codeChunk3 = ''.join(choices(ascii_uppercase+digits, k=4));
     codeChunk4 = ''.join(choices(ascii_uppercase+digits, k=4));
     produceCode = codeChunk1 + '-' + codeChunk2 + '-' + codeChunk3 + '-' + codeChunk4
-    print(produceCode)
     return produceCode
 
+"""
+validateName checks to ensure incoming name is valid
+A valid name consists of:
+    Alphanumeric chars ONLY
+    is NOT an empty string (this disqualifies as alphanumeric)
+"""
 def validateName(name):
-    isValidName = name.isalnum()
-    if isValidName:
-        return name
-    else:
-        return 400
+   isValidName = name.isalnum()
+   return isValidName
+
+"""
+formatPrice ensures incoming price is of format #.##
+    ensures price is a number
+    will add 0's or round if needed
+"""
+def formatPrice(price):
+    try:
+        numPrice = float(price)
+        return '{:.2f}'.format(numPrice)
+    except:
+        return False
 
 class ProducesList(Resource):
     def get(self):
@@ -49,14 +70,19 @@ class ProducesList(Resource):
         parser.add_argument("price")
         args = parser.parse_args()
 
-        if validateName(args["name"]) == 400:
+        formattedPrice = formatPrice(args["price"])
+
+        if not validateName(args["name"]):
+            return "Invalid request", 400
+
+        if not formattedPrice:
             return "Invalid request", 400
 
         produce_id = generateProduceCode()
         PRODUCE[produce_id] = {
             "produce code": produce_id,
             "name": args["name"],
-            "price": args["price"]
+            "price": formattedPrice
         }                
         return PRODUCE[produce_id], 201
 
